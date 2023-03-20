af863201aa67ac5da8737f80e001b354770f03c7
TrianePeart <TrianePeart@gmail.com>
2023-03-20 18:38:51 -0400

    Move Characters W| Event Listeners

--- script.js
+++ script.js
@@ -20,8 +20,8 @@ Fourth Position is height of screen
 Using OOP to make sprites interact with one another with independent properties.  
 */
 
-const gravity = 0.2
-//Giving a form of gravity 
+const gravity = 0.7
+//Giving a form of gravity handles JUMP
 
 class Sprite {
     constructor({position, velocity}){
@@ -29,6 +29,7 @@ class Sprite {
         this.position = position
         this.velocity = velocity
         this.height = 150
+        this.lastKey
         //Wrapped these in an object to hold shape of object
     }
     draw(){
@@ -41,7 +42,8 @@ class Sprite {
         //updating properties to move across the screen
         this.draw()
         //calling draw to be updated as needed
-        
+        this.position.x += this.velocity.x
+        //defining how player moves on x axis 
         this.position.y += this.velocity.y
         //using this to add px over time for each frame using OOP 
         if(this.position.y + this.height + this.velocity.y >= canvas.height){
@@ -90,6 +92,25 @@ console.log(player)
 
 //CREATE ANIMATION FILE
 
+const keys = {
+    //creating keys that control the objects within the game
+    a:{
+        pressed: false
+    },
+    d:{
+        pressed: false
+    },
+    ArrowRight:{
+        pressed:false
+    }, 
+    ArrowLeft:{
+        pressed:false
+    }
+
+}
+
+
+
 function animate(){
     window.requestAnimationFrame(animate)
     //Creating an infinite loop to show objects frame by frame to appear as if moving. Until told to stop.
@@ -99,6 +120,82 @@ function animate(){
    //opposite of fillrec
    player.update()
    op.update()
+
+   player.velocity.x = 0
+   op.velocity.x = 0
+   //controlling the direction pad 
+   if(keys.a.pressed && player.lastKey === 'a'){
+    player.velocity.x = -5
+    //Changing speed to px per frame 
+   } else if (keys.d.pressed && player.lastKey === 'd'){
+    player.velocity.x = 5
+   }
+
+   //Enemy movement 
+   if(keys.ArrowLeft.pressed && op.lastKey === 'ArrowLeft'){
+    op.velocity.x = -5
+   } else if (keys.ArrowRight.pressed && op.lastKey === 'ArrowRight'){
+    op.velocity.x = 5
+   }
+
 }
 
 animate()
+
+//PLAYER MOVEMENT
+
+window.addEventListener('keydown', (event) => {
+//PLAYER ONE
+    console.log(event.key)
+    switch(event.key){
+        case 'd':
+            keys.d.pressed = true
+            player.lastKey = 'd'
+            break
+        case 'a':
+            keys.a.pressed = true
+            player.lastKey = 'a'
+            break
+        case 'w':
+            player.velocity.y = -20
+            //changed to handle how high we jump
+            break
+    
+//PLAYER TWO 
+   
+        case 'ArrowRight':
+            keys.ArrowRight.pressed = true
+            op.lastKey = 'ArrowRight'
+            break
+        case 'ArrowLeft':
+            keys.ArrowLeft.pressed = true
+            op.lastKey = 'ArrowLeft'
+            break
+        case 'ArrowUp':
+            op.velocity.y = -20
+            break
+    }
+   
+//This is creating an event listener for when pressing down on a key.
+})
+
+window.addEventListener('keyup', (event) => {
+    switch(event.key){
+        case 'd':
+            keys.d.pressed = false
+            break
+        case 'a':
+            keys.a.pressed = false
+            break
+    }
+    //Op keys
+    switch(event.key){
+        case 'ArrowRight':
+            keys.ArrowRight.pressed = false
+            break
+        case 'ArrowLeft':
+            keys.ArrowLeft.pressed = false
+            break
+    }
+//This changes if the key is up refactor to be one switch case if possible. 
+})
